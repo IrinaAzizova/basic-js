e0d10c7ed4e867985a9489ed977f79ca3b74136c
Azizova Irina <i_r_i_n_a_nikitina@mail.ru>
2023-04-14 10:05:15 +0300

    countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  
+  let count = 0;
+
+  if (Array.isArray(matrix) && matrix.length > 0) {
+    matrix.forEach(item => {
+      item.forEach(each => {
+        if (each === '^^') {
+          count++;
+        }
+      });
+    });
+  }
+
+  return count;
 }
 
 module.exports = {
